c058659126b43e33eb150b24f46db910625644b2
lcs <cslee@itmoulds.com>
2021-01-04 10:54:18 +0900

    폼 검증하기

--- ch1/front/pages/signup.vue
+++ ch1/front/pages/signup.vue
@@ -4,29 +4,39 @@
       <v-card>
         <v-container>
           <v-subheader>회원가입</v-subheader>
-          <v-form>
+          <v-form ref="form" v-model="valid" @submit.prevent="onSubmitForm">
             <v-text-field
+                v-model="email"
                 label="이메일"
                 type="email"
+                :rules="emailRules"
                 required
             />
             <v-text-field
+                v-model="password"
                 label="비밀번호"
                 type="password"
+                :rules="passwordRules"
                 required
             />
             <v-text-field
+                v-model="password1"
                 label="비밀번호 확인"
-                type="password1"
+                type="password"
+                :rules="passwordCheckRules"
                 required
             />
             <v-text-field
+                v-model="nickname"
                 label="닉네임"
                 type="nickname"
+                :rules="nicknameRules"
                 required
             />
             <v-checkbox
+                v-model="terms"
                 label="모든 사항을 동의합니다."
+                :rules="[v=>!!v|| '약관에 동의해야 합니다.']"
                 required
             />
             <v-btn color="green" type="submit">가입하기</v-btn>
@@ -43,6 +53,36 @@ export default {
   head() {
     return {
       title: '회원가입'
+    }
+  },
+  data() {
+    return {
+      valid: false,
+      email: '',
+      password: '',
+      password1: '',
+      nickname: '',
+      terms: false,
+      emailRules: [
+        v => !!v || "이메일은 필수입니다.",
+        v => /.+@.+/.test(v) || "이메일이 유효하지 않습니다.",
+      ],
+      nicknameRules: [
+        v => !!v || "닉네임은 필수입니다.",
+      ],
+      passwordRules: [
+        v => !!v || "비밀번호는 필수입니다.",
+      ],
+      passwordCheckRules: [
+        v => v === this.password || "비밀번호가 일치하지 않습니다."
+      ]
+
+    }
+  },
+  methods: {
+    onSubmitForm() {
+      this.$refs.form.validate();
+      console.log(this.valid);
     }
   }
 }
