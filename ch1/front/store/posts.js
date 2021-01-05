7107ca69340b52f25c49c3e2c4fecf04d1151d60
lcs <cslee@itmoulds.com>
2021-01-05 13:07:57 +0900

    인피니티 스크롤링 준비

--- ch1/front/store/posts.js
+++ ch1/front/store/posts.js
@@ -1,6 +1,9 @@
 export const state = () => ({
     mainPosts: [],
+    hasMorePost: true,
 })
+
+const limit = 10;
 
 export const mutations = {
     addMainPost(state, payload) {
@@ -13,7 +16,21 @@ export const mutations = {
     addComment(state, payload) {
         const index = state.mainPosts.findIndex(v => v.id === payload.postId)
         state.mainPosts[index].Comments.unshift(payload);
-    }
+    },
+    loadPosts(state) {
+        const fakePosts = Array(limit).fill().map(v => ({
+            id: Math.random().toString(),
+            User: {
+                id: 1,
+                nickname: "더미더"
+            },
+            content: `Hello infinite scrolling - ${Math.random()}`,
+            Comments: [],
+            Images: [],
+        }))
+        state.mainPosts = state.mainPosts.concat(fakePosts);
+        state.hasMorePost = fakePosts.length === limit;
+    },
 }
 
 export const actions = {
@@ -25,5 +42,6 @@ export const actions = {
     },
     addComment({commit}, paylod) {
         commit('addComment', paylod)
-    }
+    },
+
 }
