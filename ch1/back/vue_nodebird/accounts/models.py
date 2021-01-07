c7559050cc206dec3e3b21c3898beeea579aac86
lcs <cslee@itmoulds.com>
2021-01-07 11:37:26 +0900

    added 노드버드 회원가입 - 비밀번호 암호화

--- ch1/back/vue_nodebird/accounts/models.py
+++ ch1/back/vue_nodebird/accounts/models.py
@@ -1,3 +1,4 @@
+import bcrypt
 from django.db import models
 
 
@@ -8,3 +9,11 @@ class NodeVueUser(models.Model):
     password = models.CharField(max_length=100, null=False, blank=False)
     created_at = models.DateTimeField(auto_now=False, auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True, auto_now_add=False)
+
+    def hashed_password(self, plain_password):
+        b = bcrypt.hashpw(self.password.encode('utf-8'), bcrypt.gensalt())
+        self.password = b
+
+    def match_password(self, input_password):
+        is_matched = bcrypt.checkpw(input_password.encode('utf-8'), self.password.encode('utf-8'))
+        return is_matched
--- ch1/back/vue_nodebird/accounts/serilaizers.py
+++ ch1/back/vue_nodebird/accounts/serilaizers.py
@@ -1,3 +1,4 @@
+import bcrypt
 from rest_framework import serializers
 
 from .models import NodeVueUser
@@ -11,6 +12,12 @@ class NodeBirdUserModelSerializer(serializers.ModelSerializer):
             'nickname',
             'password'
         )
-        read_only_fields = (
+        write_only_fields = (
             'password',
         )
+
+    def save(self, **kwargs):
+        validated_password = self.validated_data['password']
+        b = bcrypt.hashpw(validated_password.encode('utf-8'), bcrypt.gensalt())
+        self.validated_data['password'] = b.decode('utf-8')
+        return super().save(**kwargs)
